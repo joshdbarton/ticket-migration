735ce2743cda7ce7329a8442555cda05fd6d36c0
Josh Barton <joshdbarton@gmail.com>
2022-09-14 12:17:59 -0500

    adding throttle seconds arg

--- migrate-tickets.py
+++ migrate-tickets.py
@@ -4,14 +4,15 @@ import json
 import time
 
 parser = argparse.ArgumentParser('Migrate Issues from one GH repository to another')
-parser.add_argument(help="the <owner>/<name> repository name for the existing repo", type=str, dest='old_repo')
-parser.add_argument(help="the <owner>/<name> repository name for the new repo", type=str, dest='new_repo')
+parser.add_argument('old_repo', help="the <owner>/<name> repository name for the existing repo", type=str)
+parser.add_argument('new_repo', help="the <owner>/<name> repository name for the new repo", type=str)
+parser.add_argument('--throttle_seconds', help="number of second to wait between requests (default 5)", default=5, type=int)
 
 args = parser.parse_args()
 
 issues = json.loads(subprocess.check_output(f'gh issue list --repo {args.old_repo} --json "title,body" --limit 100'))
 for issue in issues:
     subprocess.run('gh issue create --repo {} --title "{}" --body "{}"'.format(args.new_repo, issue['title'], issue['body']))
-    time.sleep(5)
+    time.sleep(args.throttle_seconds)
 
 print('Tickets migrated!')
